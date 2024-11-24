cd2986cefd98637ccf2f802d7f7e107b9782a4c0
SerjoGG <golovnyamagadan@gmail.com>
2024-11-25 04:52:35 +1100

    Создание базовой страницы

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import routes from './routes';
-import Layout from './components/Layout';
+import routes from './routes'; //изучаем все маршруты по которым нужно отрисовать страницу
+import Layout from './components/Layout'; //отрисовщик страницы в который мы определяем какую страницу отрисовать
 
 function App() {
   const router = createBrowserRouter([
     {
-      element: <Layout />,
-      children: routes,
+      element: <Layout />, //выбираем компонент с лояутом
+      children: routes, //выбираем массив со всеми нашими путями (дочерняя отрисовка)
     },
-  ]);
+  ],
+  {
+    future: {
+      v7_startTransition: true,
+    }}
 
-  return <RouterProvider router={router} />;
+);
+
+  return <RouterProvider router={router} />; //следит за адресной строкой и определяет кую страницу отрисовать
 }
 
 export default App;
+
+
+//RouterProvider - отслеживает HTTP routes - работает как финальный рендер в который мы грузим все что должен плоучить клиент
+//createBrowserRouter - создает уже сму страницу согласно роуту
---
+++ client/src/pages/WelcomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WelcomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to our React application!</h1>
+            <p>This is a simple example of a React component.</p>
+        </div>
+    );
+}
+
+export default WelcomePage;
