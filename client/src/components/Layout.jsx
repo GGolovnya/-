81715676cbf09b8dc9e91ba2f21d71b7486bf3d4
SerjoGG <golovnyamagadan@gmail.com>
2024-11-25 05:23:13 +1100

    Навигация

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navigation from './Navigation'
 
 const Layout = () => {
   return (
     <div>
+      <Navigation />
       <main>
         <Outlet />
       </main>
+      <footer> Обучающая страница © 2024</footer>
+        
     </div>
   );
 };
---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from 'react-router-dom';
+
+function Navigation() {
+  return (
+    <nav>
+      <NavLink to="/">Главная</NavLink>
+    </nav>
+  );
+}
+
+export default Navigation;
