6209cf41b74ccdbd14395df07d9946d1f001ca47
SerjoGG <golovnyamagadan@gmail.com>
2024-11-25 02:11:57 +1100

    удаление всего

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,17 @@
 import React from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { NextUIProvider } from '@nextui-org/react';
-import Layout from './components/Layout';
 import routes from './routes';
-import setupAxiosInterceptors from './components/utils/axiosConfig';
+import Layout from './components/Layout';
 
 function App() {
-  React.useEffect(() => {
-    setupAxiosInterceptors();
-  }, []);
-
   const router = createBrowserRouter([
     {
-      path: '/',
       element: <Layout />,
       children: routes,
     },
   ]);
 
-  return (
-    <NextUIProvider>
-      <RouterProvider router={router} />
-    </NextUIProvider>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ client/src/routes/index.jsx
@@ -0,0 +1,11 @@
+
+import WelcomePage from '../pages/WelcomePage';
+
+const routes = [
+  {
+    path: '/',
+    element: <WelcomePage />,
+  },
+];
+
+export default routes;
