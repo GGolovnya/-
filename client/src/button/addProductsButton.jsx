d1e830d88fe83762c9d03ed54e1c183dd2404532
SerjoGG <golovnyamagadan@gmail.com>
2024-11-26 05:20:17 +1100

    Добавление кнпки и импута

---
+++ client/src/button/addProductsButton.jsx
@@ -0,0 +1,7 @@
+import {  Button } from "@nextui-org/react";
+
+export function AddProductsButton() {
+    return (
+      <Button>ДОБАВИТЬ ПРОДУКТ</Button>
+    );
+  }
--- client/src/pages/ContentPage.jsx
+++ client/src/pages/ContentPage.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import {Card, Input} from '@nextui-org/react'
 import {Products} from "../block/Products"
 import {Sellers} from "../block/Sellers"
+import {AddProductsButton} from "../button/addProductsButton"
 
 const ContentPage = () => {
     return (
         <div className="flex flex-col items-center justify-center space-y-6">
             <h1 className="text-2xl font-bold">Страница называется ContentPage.jsx</h1>
             <p className="text-lg">Маршрут у этой страницы /content </p>
-            <Products />
-            <Sellers/>
+            <Card className = "flex-grow mx-auto px-24 py-24 gap-4">
+                <Products />
+            </Card>
+            <Input />
+            <AddProductsButton />
+            <Card className = "flex-grow mx-auto px-24 py-24 gap-4">
+                <Sellers/>
+            </Card>
 
         </div>
     );
